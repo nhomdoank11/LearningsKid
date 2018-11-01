20610da4b7b075be4ae50253c3d10318a14e727e
nhomdoank11 <44354956+nhomdoank11@users.noreply.github.com>
2018-11-01 11:36:09 +0700

    login

--- app/src/main/java/com/example/jcj/learningskid/UserHomeContent.java
+++ app/src/main/java/com/example/jcj/learningskid/UserHomeContent.java
@@ -1,13 +1,39 @@
 package com.example.jcj.learningskid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class UserHomeContent extends AppCompatActivity {
 
+
+    String NameHolder;
+    TextView Name;
+    Button LogOUT ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home_content);
+
+        Name = findViewById(R.id.textView1);
+        LogOUT = findViewById(R.id.button1);
+
+        Intent intent = getIntent();
+        NameHolder = intent.getStringExtra(MainActivity.UserName);
+
+        Name.setText(Name.getText().toString()+ NameHolder);
+
+        LogOUT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                Toast.makeText(UserHomeContent.this,"Log Out Successfuly", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
