3f3dbdeb2ddb7d0a0ea0012e4517265754d70b38
nhomdoank11 <44354956+nhomdoank11@users.noreply.github.com>
2018-11-12 08:07:46 +0700

    them phan admin newword

---
+++ app/src/main/java/com/example/jcj/learningskid/AdminEnglishNewword.java
@@ -0,0 +1,21 @@
+package com.example.jcj.learningskid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AdminEnglishNewword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_english_newword);
+    }
+    public void gotoaddlesson(View v){
+        Intent intentadminlesson = new Intent(getApplicationContext(), AdminEnglishNewwordAddLesson.class);
+        startActivity(intentadminlesson);
+        finish();
+    }
+
+}
