58ee68484d7dc86a02ed981ab5f1fef6ce195a11
nhomdoank11 <44354956+nhomdoank11@users.noreply.github.com>
2018-11-12 21:12:13 +0700

    vinhupdate

--- app/src/main/java/com/example/jcj/learningskid/UserEnglishNewwordContent.java
+++ app/src/main/java/com/example/jcj/learningskid/UserEnglishNewwordContent.java
@@ -1,9 +1,11 @@
 package com.example.jcj.learningskid;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,6 +14,8 @@ import java.util.List;
 public class UserEnglishNewwordContent extends AppCompatActivity {
     private ListView newwordList;
     private SQLiteHelper myHelper = null;
+    private EnglishNewwordLesson lesson = new EnglishNewwordLesson();
+    int id = -2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +23,11 @@ public class UserEnglishNewwordContent extends AppCompatActivity {
         newwordList = findViewById(R.id.listWord);
         myHelper = new SQLiteHelper(getApplicationContext(),"LearningKid", 1);
         myHelper.getReadableDatabase();
+        Intent intentlessonid = getIntent();
+        id = intentlessonid.getIntExtra("lessonID",-1);
         AdapterUser_englishNewword_Content adapter = new AdapterUser_englishNewword_Content(this, select());
         newwordList.setAdapter((AdapterUser_englishNewword_Content)adapter);
+
     }
     public List<EnglishNewwordContent> select(){
         List<EnglishNewwordContent> list = new ArrayList<>();
@@ -29,7 +36,8 @@ public class UserEnglishNewwordContent extends AppCompatActivity {
         }
         SQLiteDatabase db = myHelper.getReadableDatabase();
         Cursor cursor = db.query("UserEnglishNewwordContent", new String[]{"id", "word","detail"},
-                null, null, null, null, null);
+                "lessonid = ?",  new String []{String.valueOf(id)}, null, null, null);
+
         while (cursor.moveToNext()){
             EnglishNewwordContent content = new EnglishNewwordContent(cursor.getInt(cursor.getColumnIndex("id")),
                     cursor.getString(cursor.getColumnIndex("word")),
@@ -37,5 +45,10 @@ public class UserEnglishNewwordContent extends AppCompatActivity {
             list.add(content);
         }
         return list;
+    }
+    public void backtolessonlist(View v) {
+        Intent intentbacklessonlist = new Intent(getApplicationContext(), UserEnglishNewwordLesson.class);
+        startActivity(intentbacklessonlist);
+        finish();
     }
 }
